94c99e800dfd1938901d9e46e4888abca6233dea
unknown <gsinghal2602@gmail.com>
2024-08-28 17:03:26 +0530

    aaa

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const mongoURL =  process.env.MONGODB_URL_LOCAL;
-//const mongoURL = process.env.MONGODB_URL;
+//const mongoURL =  process.env.MONGODB_URL_LOCAL;
+const mongoURL = process.env.MONGODB_URL;
 
 mongoose.connect(mongoURL
 
