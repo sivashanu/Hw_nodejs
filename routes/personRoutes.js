2337421c428c5175842d29981fe6db2e4da9e363
unknown <gsinghal2602@gmail.com>
2024-08-28 14:53:21 +0530

    path specified

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Person = require('../models/person');
+const Person = require('./../models/person');
 
 
 router.post('/',async (req,res)=>{
